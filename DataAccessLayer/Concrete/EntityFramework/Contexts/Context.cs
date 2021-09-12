724a92c2a1c36c969bdceec8d56614f2eea68eb8
atascanturk <atascanturk@gmail.com>
2021-09-13 00:36:24 +0300

    Initial commit.

--- DataAccessLayer/Concrete/EntityFramework/Contexts/Context.cs
+++ DataAccessLayer/Concrete/EntityFramework/Contexts/Context.cs
@@ -15,6 +15,7 @@ namespace DataAccessLayer.Concrete.EntityFramework.Contexts
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CoreBlogDb; Trusted_Connection=true");
         }
 
+        //DbSet
         public DbSet<About> Abouts{ get; set; }
         public DbSet<Blog> Blogs{ get; set; }
         public DbSet<Category> Categories{ get; set; }
