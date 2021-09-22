59b4e02a22fa072474af492006d5b8d9e87e88a8
atascanturk <atascanturk@gmail.com>
2021-09-23 01:21:59 +0300

    Template added. Edited with PartialViewls

---
+++ CoreDemo/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _categoryService.GetAll();
+
+            return View(categories);
+        }
+    }
+}
