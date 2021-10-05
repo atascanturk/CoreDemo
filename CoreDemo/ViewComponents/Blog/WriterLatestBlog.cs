b633ace2490a48b9c9de8e3135f3f87a574dddf4
atascanturk <atascanturk@gmail.com>
2021-10-05 15:37:09 +0300

    Register and Login processes have been completed. Validations are done.

---
+++ CoreDemo/ViewComponents/Blog/WriterLatestBlog.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class WriterLatestBlog:ViewComponent
+    {
+        IBlogService _blogService;
+
+        public WriterLatestBlog(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.GetAll(x => x.WriterId == 1).OrderByDescending(x=>x.Id).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
