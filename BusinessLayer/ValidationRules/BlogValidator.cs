e13ca4328d82451435f083e0276465a43ad32fea
atascanturk <atascanturk@gmail.com>
2021-10-23 20:25:21 +0300

    Blog adding and listing process has been set up.

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class BlogValidator :AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog için bir başlık giriniz.");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("İçerik alanını boş geçemezsiniz.");
+            RuleFor(x => x.Title).MaximumLength(150).WithMessage("Lütfen maksimum 150 karakter giriniz.");
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/Blog/BlogLast3Posts.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogLast3Posts : ViewComponent
+    {
+        IBlogService _blogService;
+
+        public BlogLast3Posts(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.GetAll().OrderByDescending(x => x.Id).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
