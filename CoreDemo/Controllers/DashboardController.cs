41cf0839023f3a8c98031079b23c531bfa4fbc71
atascanturk <atascanturk@gmail.com>
2021-11-05 23:59:17 +0300

    Writer profile editing pages has been created.

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Abstract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +10,22 @@ namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        ICategoryService _categoryService;
+        IBlogService _blogService;
+
+        public DashboardController(ICategoryService categoryService, IBlogService blogService)
+        {
+            _categoryService = categoryService;
+            _blogService = blogService;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.BlogCount = _blogService.GetAll().Count;
+            ViewBag.WriterBlogCount = _blogService.GetAll(x => x.WriterId == 33).Count;
+            ViewBag.CategoryCount = _categoryService.GetAll().Count;
+
             return View();
         }
     }
